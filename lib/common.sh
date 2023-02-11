fabb382a48e1e6c79e92c8effb27717a00d5280b
honos645 <sosuke310hns13@gmail.com>
2023-02-12 01:59:58 +0900

    FIX installer common script

--- lib/common.sh
+++ lib/common.sh
@@ -2,6 +2,6 @@
 
 [[ -n "${_COMMON}" ]] && return || readonly _COMMON=1
 
-for target in $(find ${HOME}/.dotfiles/lib -name "*.sh" -not -name "dot.*"); do
+for target in $(find lib -name "*.sh" -not -name "dot.*"); do
     source ${target}
 done
