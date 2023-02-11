88e483ce68cdf82896aa7dfdd092b72e3df35ea1
honos645 <sosuke310hns13@gmail.com>
2023-02-12 01:56:22 +0900

    FIX installer script

--- installer.sh
+++ installer.sh
@@ -4,11 +4,13 @@ if type git > /dev/null 2>&1; then
     git clone https://github.com/honos645/dotfiles
 elif type curl > /dev/null 2>&1; then
     command curl -OL https://github.com/honos645/dotfiles/archive/refs/heads/main.tar.gz
-    command tar xvf main.tar.gz -C dotfiles
+    command tar xvf main.tar.gz
+    command mv dotfiles-main dotfiles
     command rm -rf main.tar.gz
 elif type wget > /dev/null 2>&1; then
     command wget -O main.tar.gz https://github.com/honos645/dotfiles/archive/refs/heads/main.tar.gz
-    command tar xvf main.tar.gz -C dotfiles
+    command tar xvf main.tar.gz
+    command mv dotfiles-main dotfiles
     command rm -rf main.tar.gz
 else
     echo "ERROR download failure"
