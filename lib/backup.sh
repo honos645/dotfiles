915e706a5cd1457a26fad226215e5b56be8c95df
honos645 <sosuke310hns13@gmail.com>
2023-02-12 21:16:28 +0900

    UPDATE backup script

--- lib/backup.sh
+++ lib/backup.sh
@@ -4,11 +4,10 @@ function backup () {
     [[ $# -eq "0" ]] && return
     [[ -d "${HOME}/${BACKUP_DIR}" ]] || create_backup
 
-    local target=${1}
+    local target=$(readlink -f "${1}")
     command echo ${target}
 
     [[ -e "${target}" ]] || return
-    #command diff ${target} ${HOME}/${target} > /dev/null 2>&1 && [[ -L "${target}" ]] && return
-
-    command cp -rdunf ${target} ${HOME}/.dotfiles.backup/$(readlink -f ${1} | sed -e 's/\//!/g')
+    command diff ${target} ${HOME}/.dotfiles.backup/${target//\/\!} > /dev/null 2>&1 && [[ -L "${target}" ]] && return
+    command cp -rdunf ${target} ${HOME}/.dotfiles.backup/${target//\//\!}
 }
