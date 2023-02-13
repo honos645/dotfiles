019ffdcab202bd9116dbee1267308dcb188819bc
honos645 <sosuke310hns13@gmail.com>
2023-02-13 17:54:04 +0900

    FIX restore script

--- lib/restore.sh
+++ lib/restore.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-
 function restore () {
     [[ $# -eq "0" ]] && return
     [[ -d "${HOME}/${BACKUP_DIR}" ]] || create_backup
 
     local TARGET=$(readlink -f "$1")
 
-    [[ -e "${TARGET//\//\!}" ]] | return
+    [[ -e "${HOME}/.dotfiles.backup/${TARGET//\//\!}" ]] | return
     command cp -rdnf ${HOME}/.dotfiles.backup/${TARGET//\//\!} ${TARGET}
 }
