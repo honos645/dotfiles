9e93293b1bb795b6163e9240dccf493899ff9e64
honos645 <sosuke310hns13@gmail.com>
2023-07-12 05:11:30 +0900

    UPDATE dotfiles installer

--- installer.sh
+++ installer.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
 
-if [ ! -e  "${HOME}/.dotfiles" ]; then
+readonly _CHECK=0
+
+if [ ! -e "${HOME}/.local/bin/dotfiles-ctl" ]; then
     if type git > /dev/null 2>&1; then
         git clone https://github.com/honos645/dotfiles
     elif type curl > /dev/null 2>&1; then
-        command curl -OL https://github.com/honos645/dotfiles/archive/refs/heads/main.tar.gz
-        command tar xvf main.tar.gz
-        command mv dotfiles-main dotfiles
-        command rm -rf main.tar.gz
-    elif type wget > /dev/null 2>&1; then
-        command wget -O main.tar.gz https://github.com/honos645/dotfiles/archive/refs/heads/main.tar.gz
+        command curl  -C - -OL https://github.com/honos645/dotfiles/archive/refs/heads/main.tar.gz
         command tar xvf main.tar.gz
         command mv dotfiles-main dotfiles
         command rm -rf main.tar.gz
     else
-        echo "ERROR download failure"
+        echo "Download failured Require git or curl or wget"
     fi
 fi
 
 command cd ./dotfiles
 
+command ln -s ${HOME}/.local/bin/dotfiles-ctl $(pwd)/bin/dtofiles-ctl
+
 [[ -n "${_COMMON}" ]] || source lib/common.sh
 
-CONFIG=$(find . -type f -name "*.conf.sh")
+# CONFIGS=$(find . -type f -name "*.conf.sh")
 
-for config in ${CONFIG}; do
-    command source ${config}
-    $(basename ${config} .conf.sh)-install
-done
-
+# for config in ${CONFIGS}; do
+#     command source ${config}
+#     $(basename ${config} .conf.sh)-install
+# done
